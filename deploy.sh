a79cfe939d1f70ede901863750fae2f30d170eba
Luke Van Seters <lukevanseters@gmail.com>
2021-07-07 11:21:04 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-AWS_ACCOUNT_ID=401962582868
-
 docker build -t coinbase-monitor:latest .
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 docker tag coinbase-monitor:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/coinbase-monitor:latest
